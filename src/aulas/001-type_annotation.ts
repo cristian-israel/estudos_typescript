e005d93a19c620e84e3947a755e8e4d1dcb37bcd
cristian-israel <israel.dev@icloud.com>
2025-03-17 21:19:18 -0300

    ok

--- src/aulas/001-type_annotation.ts
+++ src/aulas/001-type_annotation.ts
@@ -1,1 +1,23 @@
+// Tipos básicos
 let nome: string = 'Teste';
+let idade: number = 30;
+let adulto: boolean = true;
+let simbolo: symbol = Symbol('qualquer-tipo');
+let big: bigint = 10n;
+
+// Arrays
+let arrayOfNumbers: number[] = [1, 2, 3];
+let arrayOfStrings: string[] = ['a', 'b', 'c'];
+let arrayOfNumbersGeneric: Array<number> = [1, 2, 3];
+let arrayOfStringsGeneric: Array<string> = ['a', 'b', 'c'];
+
+// Objetos
+let pessoa: { nome: string; idade: number; adulto?: boolean } = {
+  nome: 'Cristian',
+  idade: 1,
+};
+
+// Funções
+function soma(x: number, y: number){
+	 return x + y
+}
