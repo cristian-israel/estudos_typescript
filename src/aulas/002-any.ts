da098e9910e4a2d3402c52959c42a2238907aa24
cristian-israel <israel.dev@icloud.com>
2025-03-17 21:42:44 -0300

    ok

--- src/aulas/002-any.ts
+++ src/aulas/002-any.ts
@@ -1,5 +1,7 @@
+import moment from 'moment';
+
 function showMessage(msg: string) {
-  return msg
+  return moment().format('DD/MM/YYYY HH:SS:MM') + ' - ' + msg;
 }
 
-console.log(showMessage("Teste"));
+console.log(showMessage('Teste de log.'));
